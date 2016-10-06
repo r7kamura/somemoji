5c54ee9ce45595d1590bc106a3eed42f87144b54
r7kamura <r7kamura@gmail.com>
2016-10-06 20:44:28 +0900

    Add script to generate JSON files from gemojione dictionary

---
+++ script/generate_json_from_gemojione.rb
@@ -0,0 +1,22 @@
+require "fileutils"
+require "json"
+
+FileUtils.mkdir_p("data")
+specification = Gem::Specification.find_by_name("gemojione")
+json = File.read("#{specification.gem_dir}/config/index.json")
+index = JSON.parse(json)
+index.each do |shortname, data|
+  File.write(
+    "./data/#{shortname}.json",
+    JSON.pretty_generate(
+      aliases: data["aliases"].sort,
+      ascii_arts: data["aliases_ascii"].sort,
+      category: data["category"],
+      code_points_alternates: data["unicode_alternates"].sort.map { |string| string.split("-") },
+      code_points: data["unicode"].split("-"),
+      code: shortname,
+      keywords: data["keywords"].sort,
+      name: data["name"],
+    ).gsub("[\n\n  ]", "[]"),
+  )
+end
--- somemoji.gemspec
+++ somemoji.gemspec
@@ -18,5 +18,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.13"
+  spec.add_development_dependency "gemojione", "3.2.0"
+  spec.add_development_dependency "json"
   spec.add_development_dependency "rake", "~> 10.0"
 end
