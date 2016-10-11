676802f2665a865b5168ff696d31e3703975878a
r7kamura <r7kamura@gmail.com>
2016-10-11 14:14:53 +0900

    Support slop both v3 and v4

--- lib/somemoji/command_line_arguments.rb
+++ lib/somemoji/command_line_arguments.rb
@@ -45,21 +45,44 @@ module Somemoji
     # @return [Slop::Options]
     def slop_options
       @slop_options ||= begin
-        slop_options = ::Slop::Options.new
-        slop_options.banner = "Usage: somemoji extract [options]"
-        slop_options.string "-p", "--provider", "(required) apple, emoji_one, noto, or twemoji"
-        slop_options.string "-d", "--destination", "(required) directory path to locate extracted image files"
-        slop_options.string "-f", "--format", "png or svg (default: png)"
-        slop_options.integer "-s", "--size", "Some providers have different size image files"
-        slop_options.bool "-h", "--help", "Display this help message"
-        slop_options
+        if using_slop_version_4?
+          slop_options = ::Slop::Options.new
+          slop_options.banner = "Usage: somemoji extract [options]"
+          slop_options.string "-p", "--provider", "(required) apple, emoji_one, noto, or twemoji"
+          slop_options.string "-d", "--destination", "(required) directory path to locate extracted image files"
+          slop_options.string "-f", "--format", "png or svg (default: png)"
+          slop_options.integer "-s", "--size", "Some providers have different size image files"
+          slop_options.bool "-h", "--help", "Display this help message"
+          slop_options
+        else
+          ::Slop.new do
+            banner "Usage: somemoji extract [options]"
+            on "p", "provider=", "(required) apple, emoji_one, noto, or twemoji"
+            on "d", "destination=", "(required) directory path to locate extracted image files"
+            on "f", "format=", "png or svg (default: png)"
+            on "s", "size=", "Some providers have different size image files"
+            on "h", "help", "Display this help message"
+          end
+        end
       end
     end
 
     # @return [Slop::Result]
     def slop_parse_result
-      @slop_parse_result ||= ::Slop::Parser.new(slop_options).parse(@argv)
-    rescue ::Slop::UnknownOption
+      @slop_parse_result ||= begin
+        if using_slop_version_4?
+          ::Slop::Parser.new(slop_options).parse(@argv)
+        else
+          slop_options.parse!(@argv)
+          slop_options
+        end
+      end
+    rescue ::Slop::Error
+    end
+
+    # @return [Boolean]
+    def using_slop_version_4?
+      ::Slop::VERSION >= "4.0.0"
     end
   end
 end
--- somemoji.gemspec
+++ somemoji.gemspec
@@ -24,7 +24,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "gemoji", "3.0.0.rc1"
   spec.add_dependency "json"
-  spec.add_dependency "slop", ">= 4.0.0"
+  spec.add_dependency "slop"
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "gemojione", "3.2.0"
   spec.add_development_dependency "rake", "~> 10.0"
